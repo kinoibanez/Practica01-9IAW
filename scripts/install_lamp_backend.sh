99c3879b92489f401ea40dd1158166ecd282c669
Ubuntu <ubuntu@ip-172-31-62-94.ec2.internal>
2023-11-15 12:59:38 +0000

    primeros cambios

---
+++ scripts/install_lamp_backend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Esto muestra todos los comandos que se van ejecutando
+set -x 
+#Actualizamos los repositorios
+apt update
+
+#Actualizamos los paquetes de la máquina 
+
+#apt upgrade -y
+
+# Instalamos Mysql L.
+
+sudo apt install mysql-server -y
+
+
+
